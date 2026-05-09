2791dce074495eb754ca8f1b15368e0f7689023b
julierosina <julieboury@hotmail.com>
2026-05-09 16:26:33 +0200

    Update base URL configuration for production

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/kamo2/', // À modifier si le repo GitHub s'appelle autrement
+  base: process.env.NODE_ENV === 'production' ? '/kamo2/' : '/',
 })
