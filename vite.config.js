50aa9211df98b24896020dde3bb3874b75721a03
julierosina <julieboury@hotmail.com>
2026-05-09 16:15:46 +0200

    Change base URL in Vite config from '/kamo/' to '/kamo2/'

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/kamo/', // À modifier si le repo GitHub s'appelle autrement
+  base: '/kamo2/', // À modifier si le repo GitHub s'appelle autrement
 })
